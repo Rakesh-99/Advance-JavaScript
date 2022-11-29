a4d4deae9d158547ae7374ee7cc9619b21148c8d
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-11-30 00:53:33 +0530

    Question added

--- Advance Js/callbacks.js
+++ Advance Js/callbacks.js
@@ -18,20 +18,49 @@ function displayFirstName(fName, callbacks) {
 displayFirstName('Rakesh ', displayLastName)    // In this line we are passing a function in another function as an argument .
 */
 
-    function displayName(firstName, callbacks) {
-    setTimeout(function () {
-        callbacks(firstName);
-    }, 1000);
-    }
-    function displayLastName(lastName, callback) {
-    setTimeout(function () {
-        callback(lastName);
-    }, 2000);
-    }
-    let showName = displayName("Rakesh kumar", function (showName) {
-    console.log(showName);
+/*
+function displayName(firstName, callbacks) {
+  setTimeout(function () {
+    callbacks(firstName);
+  }, 1000);
+}
+function displayLastName(lastName, callback) {
+  setTimeout(function () {
+    callback(lastName);
+  }, 2000);
+}
+let showName = displayName("Rakesh kumar", function (showName) {
+  console.log(showName);
 
-    displayLastName("Parida", function (show) {
-        console.log(show);
+  displayLastName("Parida", function (show) {
+    console.log(show);
+  });
+});
+
+*/
+
+
+
+// Q.1
+// With the basis of name fetch hobbies of that person
+
+function getName(personName, callback) {
+    setTimeout(function () {
+        callback(personName);
+    }, 1000);
+};
+
+function getHobbies(personName, callback) {
+    setTimeout(function () {
+        callback(['Coding', 'Bike Riding', 'Travelling', 'Listining Music']);
+    }, 2000);
+}
+
+let fetch = getName('Rakesh', function (pName) {
+    console.log(pName);
+
+    getHobbies(pName, function (fetchHobbies) {
+        console.log(fetchHobbies);
     });
-    });
+});
+
